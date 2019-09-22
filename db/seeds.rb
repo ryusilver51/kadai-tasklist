d96b18083669a6ae55c472307e5be1d210eefd66
ryunosuke.hayashi <ryunosuke3581@gmail.com>
2019-09-22 03:26:28 +0000

    add db-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 (1..100).each do |number|
-  Task.create(status: 'test status ' + number.to_s, content: 'test content ' + number.to_s)
+  Task.create!(status: 'test status ' + number.to_s, content: 'test content ' + number.to_s)
 end
