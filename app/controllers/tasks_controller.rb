0c4b3b95a326526abf27a2f44075b86536def289
ryunosuke.hayashi <ryunosuke3581@gmail.com>
2019-09-30 06:09:28 +0000

    add post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,7 @@ class TasksController < ApplicationController
   
   def index
     if logged_in?
+      @task = current_user.tasks.build  # form_with 用
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end
   end
@@ -17,7 +18,6 @@ class TasksController < ApplicationController
   end
   
   def create
-    # @task = Task.new(task_params)
     @task = current_user.tasks.build(task_params)
 
     if @task.save
